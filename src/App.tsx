cebf5b57481cf3e104f32278974328c4001b5b62
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-09-16 08:02:58 -0300

    chore: section header

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,26 @@ import ProductCard from './components/ProductCard';
 import { products } from './products.json';
 
 const App: React.FC = () => (
-  <div className="content productsContainer">
-    {products.map((product) => (
-      <ProductCard
-        name={product.productName}
-        description={product.descriptionShort}
-        photo={product.photo}
-        price={product.price}
-      />
-    ))}
-  </div>
+  <section className="content">
+    <header>
+      <h3>
+        INSTRUMENTOS
+        {' '}
+        <b>DESTAQUE</b>
+      </h3>
+      <p>it is a long established fact that a reader will be destracted by the readable</p>
+    </header>
+    <div className="productsContainer">
+      {products.map((product) => (
+        <ProductCard
+          name={product.productName}
+          description={product.descriptionShort}
+          photo={product.photo}
+          price={product.price}
+        />
+      ))}
+    </div>
+  </section>
 );
 
 export default App;
