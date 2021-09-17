ce04ee0b5b2ae1c04ea908ca4be7a01dcb8f7fba
Eduardo Rodrigues <eduaadorfernandes@gmail.com>
2021-09-17 17:44:21 -0300

    refactor(Header): extracted header to a separated section component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 import './styles/App.scss';
-import {
-  AiOutlineLock, AiOutlinePercentage, AiOutlineWhatsApp, AiOutlineSearch, AiOutlineUser,
-} from 'react-icons/ai';
-import { FiTruck } from 'react-icons/fi';
-
-import Logo from './assets/logo.jpg';
 
 import ContentSection from './sections/ContentSection';
 import ProductsList from './sections/ProductsList';
@@ -14,6 +8,7 @@ import { products } from './products.json';
 import { text } from './aboutus.json';
 import Modal from './components/Modal';
 import useModal from './useModal';
+import Header from './sections/Header';
 
 const App: React.FC = () => {
   const {
@@ -22,64 +17,7 @@ const App: React.FC = () => {
 
   return (
     <>
-      <header className="main-header">
-        <div>
-          <ContentSection>
-            <p>
-              <AiOutlineLock size={20} />
-              <span>Compra 100%</span>
-            </p>
-            <hr />
-            <p>
-              <span>1 troca grátis</span>
-            </p>
-            <hr />
-            <p>
-              <AiOutlinePercentage size={20} />
-              <span>5x sem juros</span>
-            </p>
-            <hr />
-            <p>
-              <FiTruck size={20} />
-              <span>Entregas em todo o brasil</span>
-            </p>
-          </ContentSection>
-        </div>
-        <nav>
-          <ContentSection>
-            <h1>
-              <img src={Logo} alt="ninja som" />
-            </h1>
-            <div className="search">
-              <input type="text" placeholder="busque aqui..." />
-              <AiOutlineSearch size={24} />
-            </div>
-            <div>
-              <div>
-                <AiOutlineWhatsApp size={24} />
-                <span>(11) 99999-9999</span>
-              </div>
-              <div>
-                <AiOutlineUser size={24} />
-                <div>
-                  <span>Olá, visitante</span>
-                  <p>minha conta</p>
-                </div>
-              </div>
-            </div>
-          </ContentSection>
-          <hr />
-          <ContentSection>
-            <ul>
-              <li>Todas as categorias</li>
-              <li>Som profissional</li>
-              <li>Instrumentos musicais</li>
-              <li>promoções</li>
-              <li>contato</li>
-            </ul>
-          </ContentSection>
-        </nav>
-      </header>
+      <Header />
 
       <ContentSection>
         <header>
---
+++ src/sections/Header/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  AiOutlineLock, AiOutlinePercentage, AiOutlineSearch, AiOutlineWhatsApp, AiOutlineUser,
+} from 'react-icons/ai';
+import { FiTruck } from 'react-icons/fi';
+import ContentSection from '../ContentSection';
+import './styles.scss';
+import Logo from '../../assets/logo.jpg';
+
+const Header: React.FC = () => (
+  <header className="main-header">
+    <div>
+      <ContentSection>
+        <p>
+          <AiOutlineLock size={20} />
+          <span>Compra 100%</span>
+        </p>
+        <hr />
+        <p>
+          <span>1 troca grátis</span>
+        </p>
+        <hr />
+        <p>
+          <AiOutlinePercentage size={20} />
+          <span>5x sem juros</span>
+        </p>
+        <hr />
+        <p>
+          <FiTruck size={20} />
+          <span>Entregas em todo o brasil</span>
+        </p>
+      </ContentSection>
+    </div>
+    <nav>
+      <ContentSection>
+        <h1>
+          <img src={Logo} alt="ninja som" />
+        </h1>
+        <div className="search">
+          <input type="text" placeholder="busque aqui..." />
+          <AiOutlineSearch size={24} />
+        </div>
+        <div>
+          <div>
+            <AiOutlineWhatsApp size={24} />
+            <span>(11) 99999-9999</span>
+          </div>
+          <div>
+            <AiOutlineUser size={24} />
+            <div>
+              <span>Olá, visitante</span>
+              <p>minha conta</p>
+            </div>
+          </div>
+        </div>
+      </ContentSection>
+      <hr />
+      <ContentSection>
+        <ul>
+          <li>Todas as categorias</li>
+          <li>Som profissional</li>
+          <li>Instrumentos musicais</li>
+          <li>promoções</li>
+          <li>contato</li>
+        </ul>
+      </ContentSection>
+    </nav>
+  </header>
+);
+
+export default Header;
